9f8156a6ee478a504a880ce401fb6027eb91b3d9
Egor <61789375+code-n-cry@users.noreply.github.com>
2021-10-09 19:47:30 +0300

    Big update.
    Added new class - its name is "not_a_hole" and it is very useful cauze when I'm trying to detect holes with OpenCV, some of images are just parts of that holes and it was annoying. Now that images just go to the "not_a_hole" folder and not processed. So cauz of this I also update dataset and it'll be next commit

--- main.py
+++ main.py
@@ -1,48 +1,65 @@
 import numpy as np
-import cv2
-from imutils import contours
-import numpy as np
-from keras.preprocessing.image import load_img
-from tensorflow import keras
 import tensorflow as tf
-from keras.models import load_model
-from PIL import Image
-import os
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
 
-
-model = load_model('saved_model.h5')
-class_names = ['correct', 'wrong']
-full_image = cv2.imread(input('Path to image: '))
-original = full_image.copy()
-gray = cv2.cvtColor(full_image, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (3, 3), 0)
-canny = cv2.Canny(blurred, 120, 255, 1)
-holes_number = 0
-cnts = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-results = {'correct': 0, 'wrong': 0}
-if len(cnts) == 2:
-    cnts = cnts[0]
-else:
-    cnts = cnts[1]
-cnts, _ = contours.sort_contours(cnts, method="left-to-right")
-for c in cnts:
-    x, y, w, h = cv2.boundingRect(c)
-    holes = full_image[y: y + h + 2, x: x + w + 2]
-    hole = original[y: y + h + 2, x: x + w + 2]
-    cv2.imwrite('results/hole_{}.jpg'.format(holes_number), hole)
-    image = Image.open('results/hole_{}.jpg'.format(holes_number))
-    image = image.resize((30, 30))
-    image.save('results/hole_{}.jpg'.format(holes_number))
-    img_array = load_img(
-        'results/hole_{}.jpg'.format(holes_number), target_size=(30, 30))
-    img_array = keras.preprocessing.image.img_to_array(img_array)
-    img_array = tf.expand_dims(img_array, 0)
-    predict_img = model.predict(img_array)
-    label = tf.nn.softmax(predict_img[0])
-    os.remove('results/hole_{}.jpg'.format(holes_number))
-    image.save(f'results/{class_names[np.argmax(label)]}/hole_{holes_number}.jpg')
-    results[class_names[np.argmax(label)]] += 1
-    holes_number += 1
-print(f'Total: {holes_number} holes. {results["correct"]} are correct and {results["wrong"]} are wrong')
-print(f'Correct percentage: {round(results["correct"] / holes_number * 100, 2)}%')
-print(f'Wrong percentage: {round(results["wrong"] / holes_number * 100, 2)}%')
+data_dir = 'dataset/'
+img_width, img_height = 30, 30
+batch_size = 32
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir + 'circle_photos/',
+    validation_split=0.5,
+    subset="training",
+    seed=1337,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir + 'validate/',
+    validation_split=0.5,
+    subset="validation",
+    seed=1337,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+class_names = train_ds.class_names
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+normalization_layer = layers.experimental.preprocessing.Rescaling(1. / 255)
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+num_classes = 3
+data_augmentation = keras.Sequential(
+    [
+        layers.experimental.preprocessing.RandomFlip("horizontal",
+                                                     input_shape=(img_height,
+                                                                  img_width,
+                                                                  3)),
+        layers.experimental.preprocessing.RandomRotation(0.1),
+        layers.experimental.preprocessing.RandomZoom(0.1),
+    ]
+)
+model = Sequential([
+    data_augmentation,
+    layers.experimental.preprocessing.Rescaling(1. / 255, input_shape=(img_height, img_width, 3)),
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(num_classes)
+])
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+model.summary()
+epochs = 100
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs
+)
+model.save('saved_model.h5')
